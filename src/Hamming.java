ee48eac596028ab07873f35b6e412935d972df42
tmarthaler <tylerm@gmail.com>
2021-01-07 17:18:31 -0800

    woo

---
+++ src/Hamming.java
@@ -0,0 +1,11 @@
+public class Hamming {
+    String name;
+
+    Hamming(String name){
+        this.name=name;
+    }
+
+    public void print() {
+        System.out.println("Name: "+name);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
-package com.company;
-
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello:");
+
+        Hamming ham = new Hamming( "Correction");
+        ham.print();
     }
 }
